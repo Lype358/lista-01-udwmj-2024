e3ef955c68f9d70ae0df0fd2c47fc5e442e27691
lype <felipe071831@gmail.com>
2024-08-31 19:17:07 -0300

    Commit dos programas

---
+++ PHP/converter.php
@@ -0,0 +1,10 @@
+<?php
+// Define a temperatura em graus Celsius
+$celsius = 25.0; // Você pode alterar este valor
+
+// Converte para Fahrenheit
+$fahrenheit = ($celsius * 9/5) + 32;
+
+// Exibe o resultado
+echo "{$celsius} graus Celsius é igual a {$fahrenheit} graus Fahrenheit.\n";
+?>
